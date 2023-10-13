6410c3ab27aad37429deb4e8f0ae0359d6884202
Lucas Beauloi <l.beauloi@gmail.com>
2023-10-13 16:39:57 +0200

    feature ok

---
+++ assets/scripts/userCreateEvents.js
@@ -0,0 +1,99 @@
+// --------------- CREATION DU FORMULAIRE-----------------------------
+const parentElement = document.querySelector(".app");
+
+// creation du form
+const form = document.createElement("form");
+form.id = "create-event-form"; // on lui donne un ID
+
+// label + input pour le NOM de l'event
+const eventNameLabel = document.createElement("label");
+const eventNameInput = document.createElement("input");
+
+eventNameInput.placeholder = "Event name";
+eventNameInput.type = "text";
+eventNameInput.id = "event-name";
+
+form.appendChild(eventNameLabel); // label est enfant du form
+form.appendChild(eventNameInput); // input est enfant de form
+
+// label et input pour les DATES de l'envent
+const eventDatesLabel = document.createElement("label");
+const eventDatesInput = document.createElement("input");
+
+eventDatesInput.placeholder = "(YYYY-MM-DD)";
+eventDatesInput.type = "text";
+eventDatesInput.id = "event-dates";
+
+form.appendChild(eventDatesLabel);
+form.appendChild(eventDatesInput);
+
+// label et input pour L'AUTEUR de l'event
+const eventAuthorLabel = document.createElement("label");
+const eventAuthorInput = document.createElement("input");
+
+eventAuthorInput.placeholder = "Author";
+eventAuthorInput.type = "text";
+eventAuthorInput.id = "event-author";
+
+form.appendChild(eventAuthorLabel);
+form.appendChild(eventAuthorInput);
+
+// label et input pour LA DESCRIPTION de l'event
+const eventDescriptionLabel = document.createElement("label");
+const eventDescriptionTextarea = document.createElement("textarea");
+eventDescriptionTextarea.placeholder = "Event description";
+eventDescriptionTextarea.id = "event-description";
+
+form.appendChild(eventDescriptionLabel);
+form.appendChild(eventDescriptionTextarea);
+
+// bouton "creer event"
+const submitButton = document.createElement("button");
+submitButton.type = "submit";
+submitButton.textContent = "Create Event";
+
+form.appendChild(submitButton);
+
+// on ajoute le formulaire a parentElement (= la div app)
+parentElement.appendChild(form);
+//---------------------------------------------------------------------------------
+
+// ----------------------RECUP DES DONNEES ET ENVOI VERS L API---------------------
+const createEventForm = document.getElementById("create-event-form");
+
+createEventForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // on veut pas que l'user envoie un formulaire vide
+
+  // on recup les valeurs  du formulaire
+  const eventName = document.getElementById("event-name").value;
+  const eventDates = document.getElementById("event-dates").value.split(","); //on separe les dates avec des virgules
+  const eventAuthor = document.getElementById("event-author").value;
+  const eventDescription = document.getElementById("event-description").value;
+
+  // on cree un objet avec les données du formulaire (pour l'envoyer dans l'api apres)
+  const eventData = {
+    name: eventName,
+    dates: eventDates,
+    author: eventAuthor,
+    description: eventDescription,
+  };
+
+  // fetch puis POST pour envoyer l'objet dans l'API
+  fetch("http://localhost:3000/api/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("Événement créé avec succès.");
+      } else {
+        console.error("Erreur lors de la création de l'événement.");
+      }
+    })
+    .catch((error) => {
+      console.error("Erreur lors de la requête POST :", error);
+    });
+});
