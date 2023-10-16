a7c7022dbadc349ffc672f93aafacca0bd2db96a
Carole Gérard <gerard.carole78@gmail.com>
2023-10-16 16:54:28 +0200

    debug json

--- assets/scripts/getAllEvents.js
+++ assets/scripts/getAllEvents.js
@@ -5,7 +5,8 @@ const url = 'http://localhost:3000/api/';
 export async function getAllEvents() {
 
     // ALL EVENTS
-    const eventsResponse = await fetch(`${url}events/`);
+    // const eventsResponse = await fetch(`${url}events/`);
+    const eventsResponse = await fetch(`http://localhost:3000/api/events/`);
     const eventsJson = await eventsResponse.json();
     // console.log('All events : ', eventsJson);
 
